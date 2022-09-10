1000d0c98e82a4587b85a90134b6906bc44838e7
laellsr <lael_lsr@hotmail.com>
2022-09-10 17:48:20 -0300

    some adjusts

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import sys
+from enum import Enum
+from tokenCategory import *
+from token import *
 
-print(sys.argv[1])
+if(len(sys.argv) < 2):
+    print("Argumento de arquivo de código em perl vazio :c\nTente: python main.py <arquivo_de_codigo.pl>\n")
+    exit()
 
 try:
     file = open(sys.argv[1], 'r', encoding="utf-8")
+    
 except:
     print("Arquivo inválido")
+    
 else:
-    print("Arquivo aberto")
-    for line in file:
-        print(line)
+    print(f"Compilando {sys.argv[1]}")
+    print(file.readlines())
+    # for line, lineData in enumerate(file, start=1):
+    #     for char, charData in enumerate(lineData, start=1):
+    #         print(charData)
     # file.seek(0)
     # print(file.readlines())
+    # token = TokenCategory.COMMENT_LINE
+    # print(token)
+    
 finally:
     file.close()
